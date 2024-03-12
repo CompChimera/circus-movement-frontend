7e96cfda5eaa8391ec3b08359074f85f2c145283
Brittany Kelly <computationalchimera@gmail.com>
2024-03-12 01:44:55 -0300

    adding dist subtree

---
+++ assets/MoveSinglePage-CXz1X933.js
@@ -0,0 +1,1 @@
+import{u as a,c as o,t,a as r,o as s}from"./index-CeOzVH1C.js";import"./MoveCard.vue_vue_type_style_index_0_lang-gjAX6ghE.js";const l={__name:"MoveSinglePage",props:["id"],setup(n){const e=a();return(p,u)=>(s(),o("h2",null," Singular Move Page - "+t(r(e).params.id),1))}};export{l as default};
